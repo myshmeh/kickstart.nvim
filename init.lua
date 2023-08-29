90a83d852214554074d2d2d73c18389418ca2d5d
Yuki Misumi <u7582380@anu.edu.au>
2023-08-29 17:03:13 +1000

    added keymaps

--- init.lua
+++ init.lua
@@ -264,6 +264,10 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- [[ My own keymaps ]]
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject [V]iew' })
+vim.keymap.set('n', '<leader>it', vim.cmd.InspectTree, { desc = '[I]nspect [T]ree' })
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
